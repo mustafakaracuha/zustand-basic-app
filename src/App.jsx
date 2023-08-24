d27ffcba27a0199771700906622b16903a20f4ea
mustafakaracuha <mustafa.karacuha@detaysoft.com>
2023-08-24 14:47:50 +0300

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
 
   return (
     <div className="w-full h-screen flex flex-col items-center justify-center">
-      <div className={todos.length > 0 ? `w-auto h-[500px] p-10 bg-white shadow-xl transition-all duration-500  rounded-2xl overflow-hidden`:`w-auto h-[200px] items-center justify-center transition-all duration-500 p-10 bg-white shadow-xl rounded-2xl overflow-hidden`}>
+      <div className={todos.length > 0 ? `w-auto h-[500px] p-10 bg-white max-sm:shadow-none shadow-xl transition-all duration-500  rounded-2xl overflow-hidden`:`w-auto h-[200px] items-center justify-center transition-all duration-500 p-10 bg-white shadow-xl rounded-2xl overflow-hidden`}>
       <h1 className='text-[40px] mb-6'>Todo App with <span className='font-semibold text-sky-500'>Zustand</span></h1>
       <Form/>
       <TodoList/>
